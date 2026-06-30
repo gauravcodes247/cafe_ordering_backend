78dd13ccc46898c726f2710c33227c3a142c2962
Your Name <gauravcollege09@gmail.com>
2026-06-30 21:35:11 +0530

    feat: implement JWT authentication and protected routes

--- app/api/auth.py
+++ app/api/auth.py
@@ -2,12 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.schemas.user import UserCreate, UserResponse, UserLogin
 from sqlalchemy.orm import Session
 from app.db.dependecis import get_db
-from app.core.security import hash_password, verify_password, create_access_token
+from app.core.security import hash_password, verify_password, create_access_token, get_current_user
 from app.models.user import User
 from sqlalchemy import select
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-
 
 
 router = APIRouter(
@@ -15,8 +13,9 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
+
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-def signup(user: UserCreate, db: Session = Depends(get_db) ):
+def signup(user: UserCreate, db: Session = Depends(get_db)):
     existing_user = db.scalar(
         select(User).where(User.email == user.email)
     )
@@ -24,25 +23,25 @@ def signup(user: UserCreate, db: Session = Depends(get_db) ):
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail = "User with this email already exists !"
+            detail="User with this email already exists !"
         )
 
     existing_username = db.scalar(
-    select(User).where(User.username == user.username)
-)
+        select(User).where(User.username == user.username)
+    )
 
     if existing_username:
         raise HTTPException(
-        status_code=409,
-        detail="Username already exists",
-    )
+            status_code=409,
+            detail="Username already exists",
+        )
 
     hashed_password = hash_password(user.password)
     db_user = User(
-        username = user.username,
-        email = user.email,
-        hashed_password = hashed_password,
-        phone = user.phone
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_password,
+        phone=user.phone
     )
     db.add(db_user)
     db.commit()
@@ -50,22 +49,31 @@ def signup(user: UserCreate, db: Session = Depends(get_db) ):
 
     return db_user
 
-#login api 
+# login api
+
+
 @router.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    stmt = select(User).where(User.email == form_data.email)
+
+    stmt = select(User).where(User.email == form_data.username)
     user = db.scalar(stmt)
+
     if not user or not verify_password(
         form_data.password, user.hashed_password
     ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail = "Invalid Credentials"
+            detail="Invalid Credentials"
         )
     access_token = create_access_token(
-        data = {"sub": user.email}
+        data={"sub": user.email}
     )
     return {
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+
+@router.get("/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
--- app/core/security.py
+++ app/core/security.py
@@ -1,29 +1,80 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-from jose import jwt
-from app.core.config import settings    
+from jose import jwt, JWTError
+from fastapi import HTTPException, status
+from app.schemas.user import TokenData
+from app.core.config import settings
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.db.dependecis import get_db
+from app.models.user import User
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
-    deprecated = "auto",
+    deprecated="auto",
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def hash_password(password:str)->str:
+
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str)-> bool:
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(
         plain_password, hashed_password
     )
 
+
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc)+ timedelta(minutes=settings.access_token_expire_minutes)
-    to_encode.update({"exp":expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.secret_key, algorithm=settings.algorithm)
 
     return encoded_jwt
 
+
+def verify_access_token(token: str) -> TokenData:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could Not validate credentials",
+        headers={"WWW-AUthenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.secret_key, algorithms=[settings.algorithm]
+        )
+        email = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+
+        return TokenData(email=email)
+
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     db: Session = Depends(get_db)):
+    token_data = verify_access_token(token)
+    stmt = select(User).where(
+        User.email == token_data.email
+    )
+
+    user = db.scalar(stmt)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+
+    return user
